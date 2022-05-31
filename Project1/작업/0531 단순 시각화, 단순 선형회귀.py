3e9c59f6765e933ec95fe2696ecf96e760e28fc5
Haeinnnn <100545262+Haeinnnn@users.noreply.github.com>
2022-05-31 16:29:27 +0900

    Update and rename 0531 구별CCTV현황 시각화.py to 0531 단순 시각화, 단순 선형회귀.py

--- Project1/작업/0531 단순 시각화, 단순 선형회귀.py
+++ Project1/작업/0531 단순 시각화, 단순 선형회귀.py
@@ -13,3 +13,12 @@ plt.rcParams['figure.figsize'] = (15, 10) # 시각화 사이즈 조정
 sns.scatterplot(x="District", y="CCTV", hue="CCTV", palette="Reds", s=data["CCTV"]*10, data=data)
 # hue CCTV 개수에 따라 색을 다르게
 # S 점 사이즈
+
+plt.rcParams['font.family'] = 'NanumGothic' # 글씨 깨짐 방지 나눔고딕폰트 설치해야함
+plt.rcParams['font.size'] = 10 # 폰트 사이즈 조정
+mpl.rcParams['axes.unicode_minus'] = False # - 깨지는 현상
+plt.rcParams['figure.figsize'] = (15, 10)
+
+line = sns.lineplot(x="District", y="Crime", data=data) # 단순선형회귀식
+
+# 출처 : https://dsbook.tistory.com/52
