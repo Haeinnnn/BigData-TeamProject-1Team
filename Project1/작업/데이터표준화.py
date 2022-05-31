49bc1fea89fdc901634f11574fa7f86540a7b8d4
Haeinnnn <100545262+Haeinnnn@users.noreply.github.com>
2022-05-31 17:51:50 +0900

    Create 데이터표준화.py

---
+++ Project1/작업/데이터표준화.py
@@ -0,0 +1,11 @@
+# 데이터 표준화
+
+from sklearn.preprocessing import StandardScaler
+# Standardization 평균 0 / 분산 1
+scaler = StandardScaler()   
+scaler = scaler.fit_transform(dataa)
+
+scaler
+
+# https://mizykk.tistory.com/101
+
