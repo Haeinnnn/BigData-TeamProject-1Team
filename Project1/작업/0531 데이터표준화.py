82ae0984e1769ad37f62693b447ea2b9b7b68ff1
Haeinnnn <100545262+Haeinnnn@users.noreply.github.com>
2022-05-31 18:03:43 +0900

    Update 0531 데이터표준화.py

--- Project1/작업/0531 데이터표준화.py
+++ Project1/작업/0531 데이터표준화.py
@@ -9,3 +9,27 @@ scaler
 
 # https://mizykk.tistory.com/101
 
+from sklearn.cluster import KMeans
+# 적절한 군집수 찾기
+# Inertia(군집 내 거리제곱합의 합) number of clusters, k (적정 군집수)
+
+# elbow method 방법
+
+ks = range(1,10)
+inertias = []
+
+for k in ks:
+    model = KMeans(n_clusters=k)
+    model.fit(scaler)
+    inertias.append(model.inertia_)
+
+# Plot ks vs inertias
+plt.figure(figsize=(4, 4))
+
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('')
+plt.xticks(ks)
+plt.show()
+
+# https://planharry.tistory.com/43
