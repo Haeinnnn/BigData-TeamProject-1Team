809203418314d796f9bd9ebb9a6dbe8eaa0c7b04
Haeinnnn <100545262+Haeinnnn@users.noreply.github.com>
2022-06-07 11:42:21 +0900

    Update 06. K-Means 클러스터링.py

--- Project1/작업/보고서작성/06. K-Means 클러스터링.py
+++ Project1/작업/보고서작성/06. K-Means 클러스터링.py
@@ -1,12 +1,18 @@
+# ============================================================================================
+# 선행 패키지 설치
+# ============================================================================================
 # !pip install -U yellowbrick
-# !pip uninstall scikit-learn -y
-# !pip3 install scikit-learn
-
-from yellowbrick.cluster import KElbowVisualizer
-
+# !pip uninstall scikit-learn -y # 위의 패키지가 제대로 설치가 안될시에 언인스톨을 진행한다
+# !pip install scikit-learn
+# ============================================================================================
+# 파이썬에서 패키지를 사용하도록 설정
+# ============================================================================================
+from yellowbrick.cluster import KElbowVisualizer # Elbow Method를 이용
+# ============================================================================================
+# 자동으로 k 값을 찾아주는 수식
+# ============================================================================================
 model = KMeans()
-visualizer = KElbowVisualizer(model, n_clusters=(1,10))
-# k에서 n_clusters로 바꾸면 결과가나옴
-visualizer.fit(scaler)
+visualizer = KElbowVisualizer(model, n_clusters=(1,10)) # k의 범위를 1부터 10까지
+visualizer.fit(scaler) # 알맞은 k 값을 찾아준다
 
 # 'KMeans' object has no attribute 'k' 문구가 뜨지만 실행됨
