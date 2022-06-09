0ead1d3d861ea16e0121b5b0161d6da186ddf949
Haeinnnn <100545262+Haeinnnn@users.noreply.github.com>
2022-06-09 15:39:16 +0900

    Update 0531 단순 시각화, 단순 선형회귀.py

--- Project1/작업/0531 단순 시각화, 단순 선형회귀.py
+++ Project1/작업/0531 단순 시각화, 단순 선형회귀.py
@@ -6,7 +6,7 @@ import seaborn as sns
 data = pd.read_csv("C:/BigData/work/R_Project/ch011/Rpython/data_comb.csv", encoding="euc-kr")
 data.head()
 
-# -- 단순 시각화
+# -- 변수의 단순 시각화
 plt.rcParams['font.family'] = 'NanumGothic' # 글씨 깨짐 방지 나눔고딕폰트 설치해야함
 plt.rcParams['font.size'] = 10 # 폰트 사이즈 조정
 plt.rcParams['figure.figsize'] = (15, 10) # 시각화 사이즈 조정
@@ -15,7 +15,7 @@ sns.scatterplot(x="District", y="CCTV", hue="CCTV", palette="Reds", s=data["CCTV
 # hue CCTV 개수에 따라 색을 다르게
 # S 점 사이즈
 
-# -- 단순 선형회귀식
+# -- seaborn 단순선형회귀 시각화
 plt.rcParams['font.family'] = 'NanumGothic' # 글씨 깨짐 방지 나눔고딕폰트 설치해야함
 plt.rcParams['font.size'] = 10 # 폰트 사이즈 조정
 mpl.rcParams['axes.unicode_minus'] = False # - 깨지는 현상
@@ -23,7 +23,7 @@ plt.rcParams['figure.figsize'] = (15, 10)
 
 line = sns.lineplot(x="District", y="Crime", color='green', data=data) # 단순선형회귀식
 
-# -- matplotlib 회귀식
+# -- matplotlib 단순선형회귀 시각화
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression()
 
